4911dedf77b0063245c83067748e78743962661d
retief <retief.visser@fivium.com.au>
2017-12-05 15:29:40 +1100

    fixed typo in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test' });
-} else if (process.env.NODE_ENV === development) {
+} else if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: '.env.development' });
 }
 
@@ -44,12 +44,12 @@ module.exports = env => {
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stirngify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stirngify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_BASE_URL': JSON.stirngify(process.env.FIREBASE_BASE_URL),
-        'process.env.FIREBASE_PROCJECT_ID': JSON.stirngify(process.env.FIREBASE_PROCJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stirngify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stirngify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_BASE_URL': JSON.stringify(process.env.FIREBASE_BASE_URL),
+        'process.env.FIREBASE_PROCJECT_ID': JSON.stringify(process.env.FIREBASE_PROCJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
